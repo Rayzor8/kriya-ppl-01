635bcd28e77fd2e4b29400bb747a09ab3169d9e0
flashback220 <rayhidayat220@gmail.com>
2022-01-12 02:09:18 +0700

    restructure folder scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="main_app">
-        <h1>Rayzor dev</h1>
+    <div className="App">
+        <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/_header.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Kriya people</h1>
+        </header>
+    )
+}
+
+export default Header
